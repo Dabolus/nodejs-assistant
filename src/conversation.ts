5c31b799a7a252d798535af01bf57e3f7c8145d7
Giorgio Garasto <giorgio@garasto.it>
2018-11-16 23:17:11 +0100

    refactor: Turn Conversation into an EventEmitter

--- src/conversation.ts
+++ src/conversation.ts
@@ -1,23 +1,17 @@
+import { EventEmitter } from 'events';
 import { ClientDuplexStream } from 'grpc';
-import { AssistantLanguage } from './common';
+import { AssistantLanguage, mapAssistResponseToAssistantResponse } from './common';
 import { AssistRequest, AssistResponse, AudioOutEncoding } from './proto';
 
-export class Conversation {
-  private _onMessageCallbacks: Array<(data: AssistResponse) => void> = [];
-
+export class Conversation extends EventEmitter {
   constructor(
     private _stream: ClientDuplexStream<AssistRequest, AssistResponse>,
     private _deviceId: string,
     private _deviceModelId: string,
     public locale: AssistantLanguage,
   ) {
-    _stream.on('data', (data: AssistResponse) => {
-      this._onMessageCallbacks.forEach((callback) => callback(data));
-    });
-}
-
-  public onMessage(callback: (data: AssistResponse) => void) {
-    this._onMessageCallbacks.push(callback);
+    super();
+    this._setupEvents();
   }
 
   public send(text: string) {
@@ -37,6 +31,40 @@ export class Conversation {
         },
         textQuery: text,
       },
+    });
+  }
+
+  private _setupEvents() {
+    this._stream.on('data', (data: AssistResponse) => {
+      const mappedResponse = mapAssistResponseToAssistantResponse(data);
+      this.emit('data', mappedResponse);
+      if (mappedResponse.action) {
+        this.emit('action', mappedResponse.action);
+      }
+      if (mappedResponse.actionOnGoogle) {
+        this.emit('actionongoogle', mappedResponse.actionOnGoogle);
+      }
+      if (mappedResponse.audio) {
+        this.emit('audio', mappedResponse.audio);
+      }
+      if (mappedResponse.conversationEnded) {
+        this.emit('conversationend', mappedResponse);
+      }
+      if (mappedResponse.html) {
+        this.emit('html', mappedResponse.html);
+      }
+      if (mappedResponse.newVolume) {
+        this.emit('volume', mappedResponse.newVolume);
+      }
+      if (mappedResponse.speechRecognitionResults) {
+        this.emit('speechrecognition', mappedResponse.speechRecognitionResults);
+      }
+      if (mappedResponse.text) {
+        this.emit('message', mappedResponse.text);
+      }
+      if (mappedResponse.utteranceEnded) {
+        this.emit('utteranceend', mappedResponse);
+      }
     });
   }
 }

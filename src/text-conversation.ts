3e67301492587edd83802b4ba21b62618159e56f
Giorgio Garasto <giorgio@garasto.it>
2018-11-26 13:21:42 +0100

    docs: Add documentation for the TextConversation class

--- src/text-conversation.ts
+++ src/text-conversation.ts
@@ -1,7 +1,18 @@
 import { Conversation } from './conversation';
 import { AudioOutEncoding } from './proto';
 
+/**
+ * Represents a text conversation with the Assistant.
+ * @author Giorgio Garasto <giorgio@garasto.it>
+ * @license MIT
+ * @class
+ */
 export class TextConversation extends Conversation {
+  /**
+   * Sends a text query to the Assistant.
+   * @param text - The text query to send to the Assistant.
+   * @returns A boolean that tells whether the text query was successfully sent or not.
+   */
   public send(text: string): boolean {
     return this.sendRequest({
       audioOutConfig: {

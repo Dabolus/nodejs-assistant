8a0d89cf302289b2268a787440a81970b5ef4706
Giorgio Garasto <giorgio@garasto.it>
2018-11-26 13:24:11 +0100

    docs: Add documentation for the AudioConversation class

--- src/audio-conversation.ts
+++ src/audio-conversation.ts
@@ -3,7 +3,23 @@ import { AssistantLanguage } from './common';
 import { Conversation } from './conversation';
 import { AssistRequest, AssistResponse, AudioInConfig, AudioOutConfig } from './proto';
 
+/**
+ * Represents an audio conversation with the Assistant.
+ * @author Giorgio Garasto <giorgio@garasto.it>
+ * @license MIT
+ * @class
+ */
 export class AudioConversation extends Conversation {
+  /**
+   * Creates a new audio conversation.
+   * @param _stream - The duplex stream to use to communicate with the Assistant SDK.
+   * @param _deviceId - The device ID to use during this conversation.
+   * @param _deviceModelId - The device model ID to use during this conversation.
+   * @param locale - The locale to use during this conversation.
+   * @param audioInConfig - The audio input configuration.
+   * @param audioOutConfig - The audio output configuration.
+   * @constructor
+   */
   constructor(
     _stream: ClientDuplexStream<AssistRequest, AssistResponse>,
     _deviceId: string,
@@ -23,6 +39,11 @@ export class AudioConversation extends Conversation {
     });
   }
 
+  /**
+   * Sends audio to the Assistant.
+   * @param audio - The audio buffer to send to the Assistant.
+   * @returns A boolean that tells whether the audio buffer was successfully sent or not.
+   */
   public send(audio: Buffer): boolean {
     return this.sendRequest({ audio });
   }
